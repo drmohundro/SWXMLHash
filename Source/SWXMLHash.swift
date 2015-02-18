f2055f0aaef30194f8d632fd24aba735c59b5277
Michael Pulsifer <pulsifer.michael@dol.gov>
2015-02-18 15:49:18 -0500

    Swift 1.2 updates

--- Source/SWXMLHash.swift
+++ Source/SWXMLHash.swift
@@ -78,7 +78,7 @@ class XMLParser : NSObject, NSXMLParserDelegate {
         return XMLIndexer(root)
     }
 
-    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName: String!, attributes attributeDict: NSDictionary!) {
+/*    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName: String!, attributes attributeDict: NSDictionary!) {
 
         self.parsingElement = elementName
 
@@ -86,21 +86,31 @@ class XMLParser : NSObject, NSXMLParserDelegate {
         parentStack.append(currentNode)
 
         lastResults = ""
+    } */
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
+        self.parsingElement = elementName
+        
+        currentNode = parentStack[parentStack.count - 1].addElement(elementName, withAttributes: attributeDict)
+        parentStack.append(currentNode)
+        
+        lastResults = ""
+
     }
 
-    func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+    func parser(parser: NSXMLParser, foundCharacters string: String?) {
         if parsingElement == currentNode.name {
-            lastResults += string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            lastResults += string!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
         }
     }
 
-    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         parsingElement = elementName
-
+        
         if !lastResults.isEmpty {
             currentNode.text = lastResults
         }
-
+        
         parentStack.removeLast()
     }
 }
@@ -380,8 +390,8 @@ public class XMLElement {
         }
 
         for (keyAny,valueAny) in attributes {
-            let key = keyAny as String
-            let value = valueAny as String
+            let key = keyAny as! String
+            let value = valueAny as! String
             element.attributes[key] = value
         }
 
