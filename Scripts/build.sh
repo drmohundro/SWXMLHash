297567829e21a0d58b5b038414a95074fe823df7
David Mohundro <david@mohundro.com>
2015-10-27 20:46:11 -0500

    Get Travis CI build working
    Switched to using Xcode 7.3 and also using xcodebuild directly because
    of issues related to the hang around "Collecting info for testables"
    issue.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,4 +2,5 @@
 
 set -ev
 
-xctool -scheme "SWXMLHash iOS" clean build test -sdk iphonesimulator
+#xctool -scheme "SWXMLHash iOS" clean build test -sdk iphonesimulator
+set -o pipefail && xcodebuild -workspace SWXMLHash.xcworkspace -scheme "SWXMLHash iOS" clean build test -sdk iphonesimulator | xcpretty
